48b8c156433d53bc6eff1999682e456ee2bc9e0f
Shazil <shazilva2@gmail.com>
2024-09-06 14:16:35 +0530

    created login view

--- Backend/companyManagement/authentication/urls.py
+++ Backend/companyManagement/authentication/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view()),
+    path("login/", views.LoginView.as_view()),
 ]
--- Backend/companyManagement/authentication/views.py
+++ Backend/companyManagement/authentication/views.py
@@ -3,6 +3,9 @@ from .models import User
 from rest_framework.permissions import AllowAny
 from .serializer import RegisterSerializer
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -14,6 +17,67 @@ class RegisterView(generics.CreateAPIView):
             return Response({'message':'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
         try:
             response = super().post(request, *args,**kwargs)
-            return response
+            user = User.objects.get(email=email)
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+            return Response({'message': 'User registered successfully',
+                             'user':response.data,
+                             'access_token': access_token,
+                             'refresh_token': refresh_token},
+                             status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response(e.detail, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            try:
+                if user.is_superuser:
+                    admin_access_token = str(refresh.access_token)
+                    admin_refresh_token = str(refresh)
+                    return Response({
+                        'message': 'Admin authentication successful',
+                        'admin_access_token': admin_access_token,
+                        'admin_refresh_token': admin_refresh_token
+                    }, status=status.HTTP_200_OK)
+                
+                elif user.is_hr:
+                    hr_access_token = str(refresh.access_token)
+                    hr_refresh_token = str(refresh)
+                    return Response({
+                        'message': 'HR authentication successful',
+                        'hr_access_token': hr_access_token,
+                        'hr_refresh_token': hr_refresh_token
+                    }, status=status.HTTP_200_OK)
+                
+                elif user.is_manager:
+                    manager_access_token = str(refresh.access_token)
+                    manager_refresh_token = str(refresh)
+                    return Response({
+                        'message': 'Manager authentication successful',
+                        'manager_access_token': manager_access_token,
+                        'manager_refresh_token': manager_refresh_token
+                    }, status=status.HTTP_200_OK)
+                
+                else:
+                    employee_access_token = str(refresh.access_token)
+                    employee_refresh_token = str(refresh)
+                    return Response({
+                        'message': 'Employee authentication successful',
+                        'employee_access_token': employee_access_token,
+                        'employee_refresh_token': employee_refresh_token
+                    }, status=status.HTTP_200_OK)
+                
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({
+                'message': 'Invalid email or password'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+            
