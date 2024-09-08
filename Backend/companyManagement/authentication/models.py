6e9ffa515900ae2ce9c9c320ec0565639a80280f
Shazil <shazilva2@gmail.com>
2024-09-09 01:31:28 +0530

    created department model

--- Backend/companyManagement/authentication/models.py
+++ Backend/companyManagement/authentication/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from phonenumber_field.modelfields import PhoneNumberField
 
+class Department(models.Model):
+    departmentName = models.CharField(max_length=255)
+
 class User(AbstractUser):
     username = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
@@ -11,7 +14,8 @@ class User(AbstractUser):
     is_employee = models.BooleanField(default=True)
     phone = PhoneNumberField(null=False, blank=False, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to='images')
-    
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
--- Backend/companyManagement/companyManagement/settings.py
+++ Backend/companyManagement/companyManagement/settings.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=25),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
