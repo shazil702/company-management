2d6c698cd1a06599ead4f67e8e21de46f83ff7f1
Shazil <shazilva2@gmail.com>
2024-09-10 22:01:43 +0530

    created Attendance model

--- Backend/companyManagement/hr/models.py
+++ Backend/companyManagement/hr/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+class Attendance(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    clock_in = models.TimeField()
+    clock_out = models.TimeField(null=True, blank=True)
